9172c0307032d969570a9dc7ef2dbd77cc5386c1
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2023-09-09 13:16:00 +0530

    Update reality2.js

--- javascript/reality2.js
+++ javascript/reality2.js
@@ -1,7 +1,7 @@
-document.getElementById("img01").src = "images\realityimg1.gif";
-document.getElementById("img02").src = "images\realityimg2.jpeg";
-document.getElementById("img03").src = "images\realityimg3.png";
-document.getElementById("img04").src = "images\realityimg4.jpeg";
+document.getElementById("img01").src = "images\\real\\realityimg1.gif";
+document.getElementById("img02").src = "images\\real\\realityimg2.jpeg";
+document.getElementById("img03").src = "images\\real\\realityimg3.png";
+document.getElementById("img04").src = "images\\real\\realityimg4.jpeg";
 document.getElementById("text02").innerHTML = "In their orbits, objects travel more quickly when they are near the central body and more slowly when they are farther away.";
 document.getElementById("text03").innerHTML = "Future positions and orbits of celestial object are estimated for scheduling satellite trajectories.";
 document.getElementById("text04").innerHTML = "The heliocentric model of the solar system is proved by this law.";
