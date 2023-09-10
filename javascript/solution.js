656b745f89d85672e488512cae083cd7fbca82f8
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2023-09-10 22:17:24 +0530

    Update solution.js

--- javascript/solution.js
+++ javascript/solution.js
@@ -31,17 +31,6 @@ function setEq(){
     step2.innerHTML = text2;
     step3.innerHTML = text3;
     step4.innerHTML = text4;
-        // Change the font size for step1
-    step1.style.fontSize = '14px'; // Adjust the font size as needed
-
-    // Change the font size for step2
-    step2.style.fontSize = '14px'; // Adjust the font size as needed
-
-    // Change the font size for step3
-    step3.style.fontSize = '14px'; // Adjust the font size as needed
-
-    // Change the font size for step4
-    step4.style.fontSize = '14px'; // Adjust the font size as needed
     step2.style.visibility = "hidden";
     step3.style.visibility = "hidden";
     step4.style.visibility = "hidden";
