4aa4ae292b384b4e78c6eec15eeba13841e7aa61
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2024-05-27 03:39:48 +0530

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,5 +1,5 @@
-var desc = ["Reality", "Idealization", "Discretization", "Geometric", "Algebraic"];
-for(let i = 1; i < 6; i++)
+var desc = ["Reality", "Idealization", "Geometric", "Algebraic"];
+for(let i = 1; i < 5; i++)
 {
     let text = document.getElementById("text" + i.toString());
     text.style.visibility = "visible";
@@ -10,7 +10,6 @@ window.addEventListener("load", ()=>{
     let link = [
         "/reality.html",
         "/idealization.html",
-        "/discretization.html",
         "/geometric.html",
         "/geometric2.html",
         "/algebraic.html",
@@ -19,9 +18,9 @@ window.addEventListener("load", ()=>{
     
     let loc = window.location.pathname.replace("/Keplers-Law", "");
     let i = link.indexOf(loc);
-    if(i==4) i=3;
-    if(i == 5) i=4;
-    if(i == 6) i=4;
+    if(i==3) i=2;
+    if(i == 4) i=3;
+    if(i == 5) i=3;
     let width = d3.select(".navSvg").style("width").replace("px","");
     let xStart = parseInt(width/6);
     let xOffset = 20;
