c3353d69598fed445670537923a70a37eb3ecfae
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2024-05-27 15:14:33 +0530

    Update idealization.js

--- javascript/idealization.js
+++ javascript/idealization.js
@@ -1,6 +1,6 @@
 
 document.getElementById("left").href = "reality.html";
-document.getElementById("right").href = "geometric_a.html";
+document.getElementById("right").href = "geometric.html";
 
 function Changer(value){
     const docx = document.documentElement;
