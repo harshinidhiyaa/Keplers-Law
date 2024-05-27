66fd7bde8ab13af2721fc1d4eebfc1886e6a2d68
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2024-05-28 02:10:55 +0530

    Update idealization.js

--- javascript/idealization.js
+++ javascript/idealization.js
@@ -1,44 +1,17 @@
 function svgResize() {
-    let svgWidth = parseFloat(d3.select("#svg").style("width").replace("px", ""));
-    let marker1X = 0;
-    let marker2X = (1 / 6) * svgWidth;
-    let marker4X = (3.3 / 6) * svgWidth;
-    let marker5X = (4.4 / 6) * svgWidth;
-    let marker6X = (5.8 / 6) * svgWidth;
-
-    d3.select("#marker1").attr("x", marker1X);
-    d3.select("#marker2").attr("x", marker2X);
-    d3.select("#marker4").attr("x", marker4X);
-    d3.select("#marker5").attr("x", marker5X);
-    d3.select("#marker6").attr("x", marker6X);
+    d3.select("#marker1").attr("x", 0);
+    d3.select("#marker2").attr("x", (1 / 6) * d3.select("#svg").style("width").replace("px", ""));
+    d3.select("#marker4").attr("x", (3.3 / 6) * d3.select("#svg").style("width").replace("px", ""));
+    d3.select("#marker5").attr("x", (4.4 / 6) * d3.select("#svg").style("width").replace("px", ""));
+    d3.select("#marker6").attr("x", (5.8 / 6) * d3.select("#svg").style("width").replace("px", ""));
 }
 
 window.addEventListener("load", svgResize());
 
-// Add event listener to the left arrow button
-d3.select('.arrow.left').on("click", function() {
-    adjustSliderValue(-1); // Decrease slider value by 1
-});
-
-// Add event listener to the right arrow button
-d3.select('.arrow.right').on("click", function() {
-    adjustSliderValue(1); // Increase slider value by 1
-});
-
-// Function to adjust the slider value
-function adjustSliderValue(change) {
-    let currentValue = +d3.select('#myRange02').property("value");
-    let newValue = Math.min(Math.max(currentValue + change, 0), 100);
-    d3.select('#myRange02').property("value", newValue);
-    handleSliderInput(); // Call the handleSliderInput function to update based on the new value
-}
-
-d3.select('#myRange02').on("input", handleSliderInput);
-
-function handleSliderInput() {
-    let value = d3.select('#myRange02').property("value");
-    let max = d3.select('#myRange02').property("max");
-    let min = d3.select('#myRange02').property("min");
+d3.select('#myRange02').on("input", function() {
+    let value = d3.select(this).property("value");
+    let max = d3.select(this).property("max");
+    let min = d3.select(this).property("min");
     let length = value / (max - min);
 
     setAnim(length);
@@ -47,7 +20,7 @@ function handleSliderInput() {
     setBtn(length);
     setAxisSelection(length);
     setZoomCurve(length);
-}
+});
 
 function setAxisSelection(length) {
     if (length < 5.7 / 6)
@@ -82,13 +55,11 @@ function opacity(length, min, max) {
 function setText(length) {
     if (length < 1 / 6) {
         document.getElementById("marker2").style.fill = "#a6a5a2";
-        document.getElementById("marker3").style.fill = "#a6a5a2";
         document.getElementById("marker4").style.fill = "#a6a5a2";
         document.getElementById("marker5").style.fill = "#a6a5a2";
         document.getElementById("marker6").style.fill = "#a6a5a2";
     } else if (length > (1 / 6) && length < (2 / 6)) {
         document.getElementById("marker2").style.fill = "#5999e3";
-        document.getElementById("marker3").style.fill = "#a6a5a2";
         document.getElementById("marker4").style.fill = "#a6a5a2";
         document.getElementById("marker5").style.fill = "#a6a5a2";
         document.getElementById("marker6").style.fill = "#a6a5a2";
@@ -141,7 +112,7 @@ function setZoomCurve(length) {
         document.getElementById("earth").style.width = dim.toString() + "%";
         document.getElementById("earthContainer").style.borderRadius = radius.toString() + "%";
         document.getElementById("earth").style.top = top.toString() + "%";
-        document.getElementById("earth").style.left = left.toString() + "%";   
+        document.getElementById("earth").style.left = left.toString() + "%";
         document.getElementById("space").style.borderRadius = radius.toString() + "%";
     }
 }
@@ -158,7 +129,8 @@ function setBtn(length) {
     } else if (length > (4.4 / 6) && length < (5.8 / 6)) {
         document.getElementById("grBt").style.background = "MediumSeaGreen";
         resetButton(3);
-    } else if (length >= (5.8 / 6) && length < (5.9 / 6)) {
+    } else if (length >= (5.8 / 6
+) && length < (5.9 / 6)) {
         document.getElementById("airBt").style.background = "rgba(48, 51, 100, 0.75)";
         resetButton(4);
     }
