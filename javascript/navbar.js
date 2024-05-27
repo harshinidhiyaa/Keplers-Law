598c257e843574f05441566c37eced0532ebb0b8
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2024-05-27 18:33:19 +0530

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,11 +1,12 @@
-var desc = ["Reality", "Idealization", "h", "Geometric", "Algebraic"];
-for (let i = 1; i <= 4; i++) {
+var desc = ["Reality", "Idealization", "Geometric", "Algebraic"];
+for(let i = 1; i <= desc.length; i++)
+{
     let text = document.getElementById("text" + i.toString());
     text.style.visibility = "visible";
-    text.innerHTML = desc[i - 1];
+    text.innerHTML = desc[i-1];
 }
 
-window.addEventListener("load", () => {
+window.addEventListener("load", ()=>{
     let link = [
         "/reality.html",
         "/idealization.html",
@@ -15,17 +16,18 @@ window.addEventListener("load", () => {
         "/algebraic2.html"
     ];
     
-    let loc = window.location.pathname.replace("/Keplers-Law", "");
+    let loc = window.location.pathname.replace("/Barometric-Formula", "");
     let i = link.indexOf(loc);
-    if (i == 3) i = 2;
-    if (i == 4) i = 3;
-    if (i == 5) i = 3;
-    let width = d3.select(".navSvg").style("width").replace("px", "");
-    let xStart = parseInt(width / 6);
+    if(i==3) i=2;
+    if(i == 4) i=3;
+    if(i == 5) i=3;
+    let width = d3.select(".navSvg").style("width").replace("px","");
+    let xStart = parseInt(width/desc.length);
     let xOffset = 20;
-    let xEnd = (i + 1) * xStart + xOffset;
+    let xEnd = (i+1)*xStart + xOffset;
     xOffset = 80;
     xStart += xOffset;
+    
 
-    d3.select("#track").attr("d", "M " + xStart + " 15 H " + xEnd);
-});
+    d3.select("#track").attr("d", "M " +  xStart + " 15 H " + xEnd);
+})
