2c832c3c9546826e3660d7661691a9dab0cc7a0f
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2024-05-28 05:54:44 +0530

    Update idealization.js

--- javascript/idealization.js
+++ javascript/idealization.js
@@ -9,9 +9,11 @@ d3.select('#myRange02').on("input", function() {
 });
 
 function morphGIFs(length) {
-    // Calculate opacity for each GIF based on slider position
-    let earthOpacity = Math.max(0, Math.min(1, 1 - 3 * length));
-    let spaceOpacity = Math.max(0, Math.min(1, 3 * length - 1));
+    const maxOpacity = 1; // Maximum opacity for both elements
+
+    // Calculate opacity for each element based on slider position
+    let earthOpacity = Math.max(0, Math.min(maxOpacity, 1 - length));
+    let spaceOpacity = Math.max(0, Math.min(maxOpacity, length));
     let twobodyOpacity = Math.max(0, Math.min(1, 3 * length - 2));
 
     // Set opacity for each GIF
