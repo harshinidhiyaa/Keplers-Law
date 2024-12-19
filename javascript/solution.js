4e71ffc16abcafd9b884cffa274b3b7cc6c9dfb7
harshinidhiyaa <138139169+harshinidhiyaa@users.noreply.github.com>
2024-12-20 01:09:03 +0530

    Update solution.js

--- javascript/solution.js
+++ javascript/solution.js
@@ -15,21 +15,25 @@ function setEq(){
     let step2 = document.createElement('p');
     let step3 = document.createElement('p');
     let step4 = document.createElement('p');
+    let step5 = document.createElement('p');
     step1.setAttribute('id', 'step1');
     step2.setAttribute('id', 'step2');
     step3.setAttribute('id', 'step3');
     step4.setAttribute('id', 'step4');
+    step5.setAttribute('id', 'step5');
 // Derivation steps
     let text1 = "Use the slider to derive the equation";
-    let text2 = "Consider calculating the area of the triangle,<div class=\"der\" id=\"step\">$$ =dA = \\frac{1}{2}(r)(rd\\theta) $$</div>";
-    let text3 = "Using angular momentum formula,<div class=\"der\" id=\"stepInt\">$$ \\frac{dA}{dt}=\\frac{1}{2}\\frac{L}{m}$$</div>";
-    let text4 ="Angular momentum and mass is constant. So, $$\\frac{dA}{dt}=\\frac{1}{2}\\frac{L}{m}=constant$$ $$\\int dA = \\int \\frac{1}{2}(\\frac{L}{m})dt$$ $$A=\\frac{1}{2}\\frac{L}{m}T$$ $$A∝T$$ The region that is swept at regular intervals of time remains constant. Therefore, according to Kepler's law of equal areas, a planet's elliptical orbit around the sun causes its areal velocity to stay constant, which implies that its angular momentum likewise stays constant.";
-// The Slider
+    let text2 = "Consider calculating the area of the triangle,<div class=\"der\" id=\"step\">$$dA = \\frac{1}{2}(r)(r\\,d\\theta)$$</div>";
+    let text3 = "Using angular momentum formula, $$L = I \\omega$$";
+    let text4 = "<div class=\"der\" id=\"stepInt\">$$\\frac{dA}{dt} = \\frac{1}{2}r^2 \\omega$$</div>";
+    let text5 = "<div class=\"der\" id=\"stepFinal\">$$\\frac{dA}{dt} = \\frac{1}{2}\\frac{L}{m} = \\text{constant}$$</div>We have shown that aerial velocity is constant. This implies that the planet covers equal areas in equal time as it moves through its orbit, meaning it must move faster when closer to the Sun.";
+//The Slider 
 // Setting the inner html
     step1.innerHTML = text1;
     step2.innerHTML = text2;
     step3.innerHTML = text3;
     step4.innerHTML = text4;
+    step5.innerHTML = text5;
     step1.style.fontSize = '12px'; // Adjust the font size as needed
 
     // Change the font size for step2
@@ -40,9 +44,11 @@ function setEq(){
 
     // Change the font size for step4
     step4.style.fontSize = '12px';
+    step5.style.fontSize = '12px';
     step2.style.visibility = "hidden";
     step3.style.visibility = "hidden";
     step4.style.visibility = "hidden";
+    step5.style.visibility = "hidden";
 // Properly adding to desired parent
     $('#exp').append(divRow);
     $('#row').append(divCol1);
@@ -51,6 +57,7 @@ function setEq(){
     $('#col1').append(step2);
     $('#col1').append(step3);
     $('#col1').append(step4);
+    $('#col1').append(step5);
 //Navigation Button
     sliderReady();
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
@@ -70,29 +77,40 @@ function sliderReady()
             step2.style.visibility = "hidden";
             step3.style.visibility = "hidden";
             step4.style.visibility = "hidden";
+            step5.style.visibility = "hidden";
         }
-        else if(length > 1/4 && length < 2/4)
+        else if(length > 1/5 && length < 2/5)
         {
             document.getElementById("moreSteps2").style.visibility = "hidden";
             step2.style.visibility = "visible";
             step3.style.visibility = "hidden";
             step4.style.visibility = "hidden";
+            step5.style.visibility = "hidden";
         }
-        else if(length > 2/4 && length < 3/4)
+        else if(length > 2/5 && length < 3/5)
         {
             document.getElementById("moreSteps2").style.visibility = "hidden";
             step2.style.visibility = "visible";
             step3.style.visibility = "visible";
             step4.style.visibility = "hidden";
+            step5.style.visibility = "hidden";
         }
-        else if(length > 3/5)
+        else if(length > 3/5 && length < 4/5)
         {
             step2.style.visibility = "visible";
             step3.style.visibility = "visible";
             step4.style.visibility = "visible";
+            step5.style.visibility = "hidden";
+        }
+        else if(length > 4/5)
+            {
+                step2.style.visibility = "visible";
+                step3.style.visibility = "visible";
+                step4.style.visibility = "visible";
+                step5.style.visibility = "visible";
             d3.select(".arrowBody").style("visibility" , "visible")
             .on("click", ()=>{
-                window.location = "overview.html";
+                window.location = "11_Summary.html";
             });
         }
     });
